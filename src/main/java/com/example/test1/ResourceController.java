e3696ab4b0c02f79ba9624e2108e1f9cefdf2738
DESKTOP-26F9MF5\胡君琦 <hujunqi.1314@qq.com>
2020-08-31 20:52:14 +0800

    shouci

---
+++ src/main/java/com/example/test1/ResourceController.java
@@ -0,0 +1,18 @@
+package com.example.test1;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class ResourceController {
+
+
+    @RequestMapping(value = "/**")
+    @ResponseBody
+    public String initCount(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        return "hello word!";
+    }
+}
